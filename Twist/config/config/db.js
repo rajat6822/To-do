a89f44b411d03d4979e14a075b36fbd7a7d292b6
Rajatkashyap <rajatkashyap6822@gmail.com>
2026-05-27 17:41:35 +0530

    MongoDb connected in twist folder

---
+++ Twist/config/config/db.js
@@ -0,0 +1,12 @@
+const { default: mongoose } = require("mongoose")
+
+const connectDB = async () => {
+    try {
+
+        await mongoose.connect('mongodb://0.0.0.0/twist')
+        console.log('MongoDB connected')
+        
+    } catch (error) {
+        console.log('Error in connecting MongoDB', error)
+    }
+}
--- Twist/server.js
+++ Twist/server.js
@@ -1,4 +1,7 @@
+const connectDB = require("../SecondAttempt/src/config/db");
 const app = require("./config/app");
+
+connectDB()
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
